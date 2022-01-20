e124f7f038982d2ca8ffc433f6695901b7ff1881
Sobicus <83043123+Sobicus@users.noreply.github.com>
2022-01-20 19:57:18 +0200

    updates freecodecamp

---
+++ 70.js
@@ -0,0 +1,14 @@
+//#70
+
+function testLogicalOr(val) {
+  // Only change code below this line
+
+  if (val <= 9 || val >= 21) {
+    return "Outside";
+  }
+
+  // Only change code above this line
+  return "Inside";
+}
+
+testLogicalOr(15);
