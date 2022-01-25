2184da153bc5e2c56fd603da4fd71d878618f072
Sobicus <83043123+Sobicus@users.noreply.github.com>
2022-01-25 13:03:36 +0200

    updates freecodecamp

---
+++ 74.js
@@ -0,0 +1,19 @@
+//#74
+
+function testSize(num) {
+  // Змініть код лише під цим рядком
+  if (num < 5) {
+    return "Tiny";
+  } else if (num < 10) {
+    return "Small";
+  } else if (num < 15) {
+    return "Medium";
+  } else if (num < 20) {
+    return "Large";
+  } else {
+    return "Huge";
+  }
+  // Змініть код лише над цим рядком
+}
+
+testSize(7);
