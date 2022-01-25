d3de8380a536a3c8c44a56519ffdcc38c98f8d81
Sobicus <83043123+Sobicus@users.noreply.github.com>
2022-01-25 21:45:40 +0200

    updates freecodecamp

---
+++ 76.js
@@ -0,0 +1,23 @@
+function caseInSwitch(val) {
+  let answer = "";
+  // Змініть код лише під цим рядком
+  switch (val) {
+    case 1:
+      answer = "alpha";
+      break;
+    case 2:
+      answer = "beta";
+      break;
+    case 3:
+      answer = "gamma";
+      break;
+    case 4:
+      answer = "delta";
+      break;
+  }
+
+  // Змініть код лише над цим рядком
+  return answer;
+}
+
+caseInSwitch(1);
---
+++ 77.js
@@ -0,0 +1,23 @@
+//#77
+
+function switchOfStuff(val) {
+  let answer = "";
+  // Змініть код лише під цим рядком
+  switch (val) {
+    case "a":
+      answer = "apple";
+      break;
+    case "b":
+      answer = "bird";
+      break;
+    case "c":
+      answer = "cat";
+      break;
+    default:
+      answer = "stuff";
+  }
+  // Змініть код лише над цим рядком
+  return answer;
+}
+
+switchOfStuff(1);
