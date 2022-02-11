cc940086aa633e83cd9887a9edda0dbf6a2b6cf5
Sobicus <83043123+Sobicus@users.noreply.github.com>
2022-02-11 17:17:55 +0200

    updates Freecodecamp

--- 86.js
+++ 86.js
@@ -1,9 +1,11 @@
-let a = 'ssss';
-let b = [];
-b = [1,2,3,4,5,6,7,8,9]
-function lol(a,b){
-    function test(a,b){
-        return a-b
-    }
-    return a+b
-}
+//#86
+// setup
+const testObj = {
+    12: "Namath",
+    16: "Montana",
+    19: "Unitas"
+};
+
+// Change code below this line
+const playerNumber = 16;  // Change this line
+const player = testObj[playerNumber];   // Change this line
