aead0c645c992ba6c548a004610606d11a46ec40
Sobicus <83043123+Sobicus@users.noreply.github.com>
2022-02-11 17:05:32 +0200

    test WS

---
+++ 85.js
@@ -0,0 +1,12 @@
+//#85
+
+// Setup
+const testObj = {
+    "an entree": "hamburger",
+    "my side": "veggies",
+    "the drink": "water"
+  };
+  
+  // Change code below this line
+  const entreeValue = testObj["an entree"];    // Change this line
+  const drinkValue = testObj["the drink"];     // Change this line
---
+++ 86.js
@@ -0,0 +1,9 @@
+let a = 'ssss';
+let b = [];
+b = [1,2,3,4,5,6,7,8,9]
+function lol(a,b){
+    function test(a,b){
+        return a-b
+    }
+    return a+b
+}
